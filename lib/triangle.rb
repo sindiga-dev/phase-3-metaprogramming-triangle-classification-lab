d2ab3522ea3a3acb706bb2a12da554b5c7345973
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-26 10:57:08 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+# Triangle#kind returns :equilateral when all sides are equal
+# Triangle#kind returns :isosceles when two sides are equal
+# Triangle#kind returns :scalene when no sides are equal
+# Triangle#kind raises an error for triangles with no size
+# Triangle#kind raises an error for triangles with no size
+# Triangle#kind raises an error for triangles violating triangle inequality
+
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a + b <= c || a + c <= b || b + c <= a
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
